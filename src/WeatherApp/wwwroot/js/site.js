a3083f13dbdb85256b65d37bd9166dbe478f161b
Maciek Malik <maciek.malik@live.com>
2022-06-01 00:18:24 +0200

    Some fixes.

--- src/WeatherApp/wwwroot/js/site.js
+++ src/WeatherApp/wwwroot/js/site.js
@@ -2,10 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-jQuery.extend(jQuery.validator.methods, {
-    number: function (value, element) {
-        return this.optional(element)
-            || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:[,.]\d+)?$/.test(value);
-    }
-});
